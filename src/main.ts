f92334c1e0cfbd61dd6cecaf92231ad8ce57c679
PK <monpushp@gmail.com>
2025-07-14 09:57:33 +0530

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowser } from '@angular/platform-browser'
+import { defineCustomElements } from '@ionic/pwa-elements/loader'
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+defineCustomElements(window);
+
+platformBrowser().bootstrapModule(AppModule)
   .catch(err => console.log(err));
