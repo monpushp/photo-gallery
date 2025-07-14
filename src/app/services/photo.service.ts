895a3af245e8074f8171402528048b46cabbf508
PK <monpushp@gmail.com>
2025-07-14 11:02:37 +0530

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   src/app/services/photo.service.ts

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,31 +1,58 @@
 import { Injectable } from '@angular/core';
-import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import {
+  Camera,
+  CameraResultType,
+  CameraSource,
+  Photo,
+} from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PhotoService {
-
-  constructor() { }
-
+  constructor() {}
   public photos: UserPhoto[] = [];
-
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+      reader.readAsDataURL(blob);
+    });
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    };
+  }
   public async addNewToGallery() {
-    // Take a photo
     const capturePhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
-      quality: 100
+      quality: 100,
     });
-
+    const savedImageFile = await this.savePicture(capturePhoto);
     this.photos.unshift({
-      filePath: "soon...",
-      webviewPath: capturePhoto.webPath!
+      filePath: savedImageFile.filepath,
+      webviewPath: savedImageFile.webviewPath,
     });
   }
-
 }
 
 export interface UserPhoto {
